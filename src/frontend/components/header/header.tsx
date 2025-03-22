d8fbd579562a2bd40eb5c07c100944ef67fef089
rafaelhgreco <zrafaelgreco@gmail.com>
2025-03-22 00:05:13 -0300

    feat: dropdown menu

--- src/frontend/components/header/header.tsx
+++ src/frontend/components/header/header.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import "./header.css";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 function Header() {
     return (
@@ -42,30 +42,37 @@ function Header() {
 }
 
 function Dropdown({ title, options }: { title: string; options: string[] }) {
-    const [open, setOpen] = useState(false);
-
     return (
-        <div className="relative">
-            <button
-                className="text-gray-700 font-medium hover:text-blue-500"
-                onClick={() => setOpen(!open)}
-            >
+        <Menu as="div" className="relative">
+            <Menu.Button className="text-gray-700 font-medium hover:text-blue-500">
                 {title}
-            </button>
-            {open && (
-                <ul className="absolute bg-white shadow-md mt-2 rounded-lg py-2 w-40">
+            </Menu.Button>
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="absolute mt-2 w-48 origin-top-right bg-white rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     {options.map((option, index) => (
-                        <li
-                            key={index}
-                            className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                            onClick={() => setOpen(false)}
-                        >
-                            {option}
-                        </li>
+                        <Menu.Item key={index}>
+                            {({ active }) => (
+                                <button
+                                    className={`${
+                                        active ? "bg-gray-100" : ""
+                                    } text-gray-700 block w-full px-4 py-2 text-left`}
+                                >
+                                    {option}
+                                </button>
+                            )}
+                        </Menu.Item>
                     ))}
-                </ul>
-            )}
-        </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
     );
 }
 
