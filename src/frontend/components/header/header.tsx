b9dc8f00cd07b7347d4c55bea4cc21f6b9bb3e2a
rafaelhgreco <zrafaelgreco@gmail.com>
2025-03-29 20:18:48 -0300

    header

--- src/frontend/components/header/header.tsx
+++ src/frontend/components/header/header.tsx
@@ -1,85 +1,85 @@
-import {
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuItems,
-    Transition,
-} from "@headlessui/react";
-import { Fragment } from "react";
-
-interface DropdownProps {
-    title: string;
-    options: string[];
-}
+import { Link } from "react-router-dom";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
 
 export const Header = () => {
+    // Dados dos menus
+    const navItems = [
+        {
+            title: "Serviços",
+            links: [
+                { name: "Opção 1", path: "/servicos/opcao1" },
+                { name: "Opção 2", path: "/servicos/opcao2" },
+            ],
+        },
+        {
+            title: "Sobre",
+            links: [
+                { name: "Nossa História", path: "/sobre/historia" },
+                { name: "Equipe", path: "/sobre/equipe" },
+            ],
+        },
+        {
+            title: "Contato",
+            links: [
+                { name: "Email", path: "/contato/email" },
+                { name: "Localização", path: "/contato/localizacao" },
+            ],
+        },
+    ];
+
     return (
         <header className="bg-white shadow-md py-4">
             <div className="container mx-auto flex items-center justify-between px-6">
                 <nav className="flex space-x-6">
-                    <Dropdown
-                        title="Serviços"
-                        options={["Opção 1", "Opção 2", "Opção 3"]}
-                    />
-                    <Dropdown
-                        title="Sobre"
-                        options={["Nossa História", "Equipe", "Missão"]}
-                    />
-                    <Dropdown
-                        title="Contato"
-                        options={["Email", "Telefone", "Localização"]}
-                    />
+                    {/* Links simples */}
+                    <Link
+                        to="/"
+                        className="self-center text-gray-700 hover:text-blue-500"
+                    >
+                        Home
+                    </Link>
+
+                    {/* Dropdowns */}
+                    {navItems.map((item) => (
+                        <Menu key={item.title} as="div" className="relative">
+                            <MenuButton className="text-gray-700 hover:text-blue-500">
+                                {item.title}
+                            </MenuButton>
+                            <MenuItems className="absolute mt-2 w-48 bg-white shadow-lg rounded-md z-10">
+                                {item.links.map((link) => (
+                                    <MenuItem key={link.path}>
+                                        <Link
+                                            to={link.path}
+                                            className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                                        >
+                                            {link.name}
+                                        </Link>
+                                    </MenuItem>
+                                ))}
+                            </MenuItems>
+                        </Menu>
+                    ))}
                 </nav>
 
                 <div className="flex-3 flex justify-center items-center">
-                    <img
-                        src="/images/flag-marrocos.jpg"
-                        alt="Logo"
-                        className="h-20 rounded-lg"
-                    />
+                    <Link to="/">
+                        <img
+                            src="/images/flag-marrocos.jpg"
+                            alt="Logo"
+                            className="h-20 rounded-lg"
+                        />
+                    </Link>
                 </div>
 
                 <div className="flex-1 flex justify-end">
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+                    <Link
+                        to="/duvidas"
+                        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+                    >
                         Tirar Dúvida
-                    </button>
+                    </Link>
                 </div>
             </div>
         </header>
-    );
-};
-
-const Dropdown: React.FC<DropdownProps> = ({ title, options }) => {
-    return (
-        <Menu as="div" className="relative">
-            <MenuButton className="text-gray-700 font-medium hover:text-blue-500">
-                {title}
-            </MenuButton>
-            <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-            >
-                <MenuItems className="absolute mt-2 w-48 origin-top-right bg-white rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    {options.map((option, index) => (
-                        <MenuItem key={index}>
-                            {({ active }: { active: boolean }) => (
-                                <button
-                                    className={`${
-                                        active ? "bg-gray-100" : ""
-                                    } text-gray-700 block w-full px-4 py-2 text-left`}
-                                >
-                                    {option}
-                                </button>
-                            )}
-                        </MenuItem>
-                    ))}
-                </MenuItems>
-            </Transition>
-        </Menu>
     );
 };
