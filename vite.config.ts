5e29e950d8df1b8cbbac95b6752e8bcc50098d5d
rafaelhgreco <zrafaelgreco@gmail.com>
2025-03-29 23:38:33 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     build: {
         outDir: "src/frontend/dist",
+        emptyOutDir: true,
     },
     base: "/volta-ao-mundo-marrocos/",
 });
